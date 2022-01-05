08380712ec4b3c0a14b52618b0fad64873536ed2
Marcos Miklos <marcos.rmt@gmail.com>
2022-01-04 22:04:10 -0300

    refactory: spliting in sidebar and content components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,38 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+import '../styles/content.scss';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Array<MovieProps>;
+  selectedGenre: {
+    title: string;
+  }
+}
+export function Content(props: ContentProps) {
   // Complete aqui
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  );
 }
